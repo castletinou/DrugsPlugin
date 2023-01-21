e51c00b86e226e3bec469fc72c5d3e3b9cd2b205
castletinou <castletinou48@gmail.com>
2023-01-21 12:36:36 +0100

    Ajout de drogue + correction bug

--- src/fr/castletinou/plugin/CommandDrugs.java
+++ src/fr/castletinou/plugin/CommandDrugs.java
@@ -7,10 +7,8 @@ import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -25,33 +23,62 @@ public class CommandDrugs implements CommandExecutor {
 			
 			if (args[0].equalsIgnoreCase("MIH")) {
 				
-				player.getInventory().addItem(getItem(Material.POTION, "§9§kI§r§d§l MIH §r§9§kI", "", "§lThis potion give you §b§lSpeed V during §a§l30 seconds"));
+				player.getInventory().addItem(getItem(Material.POTION, "§9§kI§r§d§l MIH §r§9§kI", "", "This potion give you §bSpeed V during §a30 seconds"));
 
 				player.updateInventory();
 			}
 			if (args[0].equalsIgnoreCase("sankukai")) {
 
-				player.getInventory().addItem(getItem(Material.POTION, "§3§lSan Ku Kaï", "", "§lThis potion give you §c§lStrength III during §a§l15 seconds"));;
+				player.getInventory().addItem(getItem(Material.POTION, "§3§lSan Ku Kaï", "", "This potion give you §cStrength III during §a15 seconds"));;
 
 				player.updateInventory();
+			}		
+			if (args[0].equalsIgnoreCase("berseker")) {
+				
+				player.getInventory().addItem(getItem(Material.POTION, "§c§ki§4§ki§r§4§l Berseker §4§ki§c§ki", "", "This potion give you a soul of viking"));;
+				
+				player.updateInventory();
 			}
-			if (args[0].equalsIgnoreCase("prestinox")) {
-
-				player.getInventory().addItem(getItem(Material.GOLDEN_CARROT, "§2§lPrestinox", "", "§lThis potion give you §c§lStrength II, §b§lSpeedIII and §a§lhasteII §d§lduring 30 second"));;
-
+			if (args[0].equalsIgnoreCase("LuckyDrug")) {
+				
+				player.getInventory().addItem(getItem(Material.POTION, "§e§lLucky Drug", "", "This potion give you a §aBonus §5or a §4Malus §5during §a30 seconds"));;
+				
 				player.updateInventory();
 			}
 			
+
+			if (args[0].equalsIgnoreCase("prestinox")) {
+
+				player.getInventory().addItem(getItem(Material.POTION, "§2§lPrestinox", "", "This potion give you §cStrength II, §bSpeedIII and §ahasteII §dduring 30 second"));;
+
+				player.updateInventory();
+			}
+			if (args[0].equalsIgnoreCase("trambolone")) {
+
+				player.getInventory().addItem(getItem(Material.POTION, "§b§lTrambolone", "", "This potion give you §aJump10 §rbe careful !"));;
+
+				player.updateInventory();
+			}
+			if (args[0].equalsIgnoreCase("misterfreeze")) {
+
+				player.getInventory().addItem(getItem(Material.POTION, "§9§kI§b§kI§r§9§l Mister Freeze §9§kI§b§kI", "", "This potion give you §9Freeze V but you're very resistant !"));;
+
+				player.updateInventory();
+			}
 			
 			if (args[0].equalsIgnoreCase("admin")) {
 				
 				Inventory inv = Bukkit.createInventory(null, 9, "§8Drugs Menu");				
-				inv.setItem(2, getItem(Material.GOLDEN_CARROT, "§2§lPrestinox", "", "§lThis potion give you §c§lStrength II, §b§lSpeedIII §5§o§land §a§lhasteII §5§o§lduring §d§l30 second"));;
-				inv.setItem(0, getItem(Material.POTION, "§3§lSan Ku Kaï", "", "§lThis potion give you §c§lStrength III §5§o§lduring §a§l15 seconds"));
-				inv.setItem(1, getItem(Material.POTION, "§9§kI§r§d§l MIH §r§9§kI", "", "§lThis potion give you §b§lSpeed V §5§o§lduring §a§l30 seconds"));
+				
+				inv.setItem(0, getItem(Material.POTION, "§3§lSan Ku Kaï", "", "This potion give you §cStrength III during §a15 seconds"));
+				inv.setItem(1, getItem(Material.POTION, "§9§kI§r§d§l MIH §r§9§kI", "", "This potion give you §bSpeed V during §a30 seconds"));
+				inv.setItem(2, getItem(Material.POTION, "§2§lPrestinox", "", "This potion give you §cStrength II, §bSpeedIII and §ahasteII §dduring 30 second"));
+				inv.setItem(3, getItem(Material.POTION, "§e§lLucky Drug", "", "This potion give you a §aBonus §5or a §4Malus §5during §a30 seconds"));
+				inv.setItem(4, getItem(Material.POTION, "§b§lTrambolone", "", "This potion give you §aJump 20 §5during §a5 seconds §5be careful !"));
+				inv.setItem(5, getItem(Material.POTION, "§9§kI§b§kI§r§9§l Mister Freeze §9§kI§b§kI", "", "This potion give you §9Freeze V §5but you're very §8resistant !"));
+				inv.setItem(6, getItem(Material.POTION, "three chance", "", "3"));
+				
 				player.openInventory(inv);
-				
-				
 			}
 
 			return true;
@@ -59,19 +86,14 @@ public class CommandDrugs implements CommandExecutor {
 
 		return false;
 	}
+	
 	public ItemStack getItem(Material material, String customName, String customLore, String customLore2) {
 		ItemStack it = new ItemStack(material, 1);
 		ItemMeta itM = it.getItemMeta();
 		itM.setDisplayName(customName);
 		itM.setLore(Arrays.asList(customLore, customLore2));
-		itM.addEnchant(Enchantment.ARROW_DAMAGE, 1, true);
-		itM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
 		it.setItemMeta(itM);
 		return it;
-		
-		
-		
-		
 	}
 	
 	
--- src/fr/castletinou/plugin/MyListeners.java
+++ src/fr/castletinou/plugin/MyListeners.java
@@ -1,5 +1,6 @@
 package fr.castletinou.plugin;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -8,32 +9,80 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-import net.minecraft.server.v1_8_R3.Enchantment;
-
 public class MyListeners implements Listener {
 	@EventHandler
-    public void onPotionDrink(PlayerItemConsumeEvent event) {
-		
-        Player player = event.getPlayer();
-        ItemStack item = event.getItem();
-        String name = item.getItemMeta().getDisplayName();
+	public void onPotionDrink(PlayerItemConsumeEvent event) {
         
-        
-        if (name == "§9§kI§r§d§l MIH §r§9§kI" && Enchantment.getEffects() != null) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 4));
-            player.sendMessage("§l[Drugs] §bGiving 30s of Speed V ... Made in Heaven !");
-        }
-        
-        if (name == "§3§lSan Ku Kaï" && Enchantment.getEffects() != null) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 300, 2));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 2));
-            player.sendMessage("§l[Drugs] §cGiving 15 s of Strength III !");
-        }
-        if (name == "§2§lPrestinox" && Enchantment.getEffects() != null) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 1));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 2));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 600, 1));
-            player.sendMessage("§l[Drugs] §aGiving 30 s of Strength II , SpeedIII and HasteII !");
-        }
-    }
+		Player player = event.getPlayer();
+		ItemStack item = event.getItem();
+
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§9§kI§r§d§l MIH §r§9§kI")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 4));
+			player.sendMessage("§l[Drugs] §bGiving 30s of Speed V ... Made in Heaven !");
+		}
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§3§lSan Ku Kaï")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 300, 2));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 100, 2));
+			player.sendMessage("§l[Drugs] §cGiving 15 s of Strength III !");
+		}
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§c§ki§4§ki§r§4§l Berseker §4§ki§c§ki")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 300, 5));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 300, 2));
+			player.sendMessage("§l[Drugs] §cGiving 15 s of Strength III !");
+		}
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§2§lPrestinox")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 600, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 2));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.FAST_DIGGING, 600, 1));
+			player.sendMessage("§l[Drugs] §aGiving 30 s of Strength II , SpeedIII and HasteII !");
+		}
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§b§lTrambolone")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 100, 20));
+			player.sendMessage("§l[Drugs] §dGiving 5 s of Jump XX !");
+		}
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§9§kI§b§kI§r§9§l Mister Freeze §9§kI§b§kI")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 300, 8));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 300, 4));
+			player.sendMessage("§l[Drugs] §9Giving 15 s of Freeze V and resi V ");
+		}
+
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§e§lLucky Drug")) {
+
+			int randint = (int) (Math.random() * (3 + 1));
+
+			if (randint == 0) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 600, 6));
+				player.sendMessage("§4§lBad Luck ...§r Gave you §8§lBlindness VI§r for §b30 seconds§r.");
+			}
+
+			if (randint == 1) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 300, 7));
+				player.sendMessage("§4§lBad Luck ...§r Gave you §2§lNausea VII§r for §b30 seconds§r.");
+			}
+
+			if (randint == 2) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 600, 5));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 20));
+				player.sendMessage("§a§lLucky !§r Gave you §c§lHealth Boost V§r for §b30 seconds§r.");
+			}
+
+			if (randint == 3) {
+				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 3));
+				player.sendMessage("§a§lLucky !§r Gave you §d§lRegeneration IV§r for §b30 seconds§r.");
+			}
+		}
+	}
 }
