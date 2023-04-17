91a6822334c1d121a301a8d24186392c5a7dcca9
castletinou <castletinou48@gmail.com>
2023-04-17 17:24:00 +0200

    Réglage bug

--- src/fr/castletinou/plugin/CommandDrugs.java
+++ src/fr/castletinou/plugin/CommandDrugs.java
@@ -31,10 +31,10 @@ public class CommandDrugs implements CommandExecutor {
 				inv.setItem(0, createPotion(Material.POTION, "§3§lSan Ku Kaï", "", "§7This potion give you §c§lStrength III§7 during §a§l15 seconds","§6Legendary", PotionType.STRENGTH));
 				inv.setItem(1, createPotion(Material.POTION, "§9§kI§r§d§l MIH §r§9§kI", "", "§7This potion give you §b§lSpeed V§7 during §a§l30 seconds","§5Epic", PotionType.SPEED));
 				inv.setItem(2, createPotion(Material.POTION, "§2§lPrestinox", "", "§7This potion give you §c§lStrength II§7, §b§lSpeed III§7 and §a§lHaste II§7 during §d§l30 seconds","§6Legendary", PotionType.REGEN));
-				inv.setItem(3, createPotion(Material.POTION, "§e§lLucky Drug", "", "§7This potion give you a §a§lBonus§7 or a §4§lMalus§7 during §a§l30 seconds","§9Rare", PotionType.WEAKNESS));
+				inv.setItem(3, createPotion(Material.POTION, "§e§lLucky Drug", "", "§7This potion give you a §a§lBonus§7 or a §4§lMalus§7 during §a§l30 seconds","§aCommon", PotionType.WEAKNESS));
 				inv.setItem(4, createPotion(Material.POTION, "§b§lTrambolone", "", "§7This potion give you §aJump 20 §7during §a5 seconds §7be careful !","§9Rare", PotionType.JUMP));
-				inv.setItem(5, createPotion(Material.POTION, "§9§kI§b§kI§r§9§l Mister Freeze §9§kI§b§kI", "", "§7This potion give you §9Freeze V §7but you're very §8resistant !", "§9Rare", PotionType.WATER));
-				inv.setItem(6, createPotion(Material.POTION, "§c§ki§4§ki§r§4§l Berseker §4§ki§c§ki", "", "§7This potion give you a §5soul §7of §4viking","§5Epic", PotionType.STRENGTH));
+				inv.setItem(5, createPotion(Material.POTION, "§9§kI§b§kI§r§9§l Mister Freeze §9§kI§b§kI", "", "§7This potion give you §9Freeze V §7but you're very §8resistant !", "§5Epic", PotionType.WATER));
+				inv.setItem(6, createPotion(Material.POTION, "§c§ki§4§ki§r§4§l Berseker §4§ki§c§ki", "", "§7This potion give you a §5soul §7of §4viking","§6Legendary", PotionType.STRENGTH));
 				inv.setItem(7, createSplashPotion(Material.POTION,"§6§lBusu§r §aCommon", "§rThis potion stun all the entities in", "§rits effect area for §c§l2 seconds§r.", "§aCommon", PotionType.SLOWNESS));
 				inv.setItem(8, createSplashPotion(Material.POTION,"§6§lBusu§r §9Rare", "§rThis potion stun all the entities in", "§rits area of effect for §c§l5 seconds§r.", "§9Rare", PotionType.SLOWNESS));
 				inv.setItem(9, createSplashPotion(Material.POTION,"§b§lBleacher", "§rThis potion §c§lclear the potions effects", "§rof any entities in its area of effect.", "§9Rare", PotionType.INVISIBILITY));
@@ -42,10 +42,7 @@ public class CommandDrugs implements CommandExecutor {
 				inv.setItem(11, createPotion(Material.POTION,"§3§lSapho Juice", "§rThis potion give §b§lSpeed III§r  but also", "§r§7§lWeakness VI§r and §6§lMining Fatigue III§r.", "§aCommon", PotionType.SPEED));
 				inv.setItem(12, createPotion(Material.POTION,"§a§lPlomik Soup", "§rThis potion give §a§lJump Boost XXX§r for a short time","§rbut also §2§lNausea III§r and §8§lBlindness III§r.", "§aCommon", PotionType.JUMP));
 				inv.setItem(13, createSplashPotion(Material.POTION,"§d§lDouble Heal", "§rThis potion act as a Splash Potion of Healing", "§rbut heals §c§l10 hearts§r.", "§aCommon", PotionType.INSTANT_HEAL));
-				inv.setItem(14, createPotion(Material.POTION,"§6§lDivine Nectar", "§rThis Nectar will §c§linstantly regenerate§r your §6§lFood Bar", "§rand gives §d§lRegeneration I§r during 1 minute.", "§aCommon", PotionType.INSTANT_HEAL));
-				inv.setItem(15, createPotion(Material.POTION,"§f§lPale Potion", "§rThis potion will give you §7§lInvisibility II§r §c§lfor 30 minutes§r,", "§rhowever you'll also have §8§lSlowness I§r.", "§9Rare", PotionType.INVISIBILITY));
-				inv.setItem(16, createPotion(Material.POTION,"§6§lSandy's Nap Time", "§rThis potion make you sleep well.", "§rDuring §c§l10 seconds§r, you are §c§linvincible§r but you §c§lcan't moove§r.", "§5Epic", PotionType.NIGHT_VISION));
-				
+				inv.setItem(14, createPotion(Material.POTION,"§e§lAmbroisie", "","§rThis potion give §d§lRegen I§r, §8Resistance II §7 and full heal yourself", "§aCommon", PotionType.NIGHT_VISION));
 				player.openInventory(inv);
 			}
 			
--- src/fr/castletinou/plugin/MyListeners.java
+++ src/fr/castletinou/plugin/MyListeners.java
@@ -20,18 +20,18 @@ import Task.TaskHealthChangeRottenHeal;
 import net.minecraft.server.v1_8_R3.Enchantment;
 
 public class MyListeners implements Listener {
-	
+
 	public static List<Entity> player_list_RottenHeal = new ArrayList<Entity>();
 	public static List<Entity> player_list_Bleacher = new ArrayList<Entity>();
+
 	@EventHandler
-    public void onPotionDrink(PlayerItemConsumeEvent event) {
-		
-        Player player = event.getPlayer();
-        ItemStack item = event.getItem();
-        String name = item.getItemMeta().getDisplayName();
-        
-        
-        if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+	public void onPotionDrink(PlayerItemConsumeEvent event) {
+
+		Player player = event.getPlayer();
+		ItemStack item = event.getItem();
+		String name = item.getItemMeta().getDisplayName();
+
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
 				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§9§kI§r§d§l MIH §r§9§kI")) {
 			player.removePotionEffect(PotionEffectType.SPEED);
 			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 4));
@@ -40,6 +40,7 @@ public class MyListeners implements Listener {
 
 		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
 				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§3§lSan Ku Kaï")) {
+			player.removePotionEffect(PotionEffectType.INCREASE_DAMAGE);
 			player.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, 300, 2));
 			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 200, 2));
 			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 0));
@@ -77,160 +78,177 @@ public class MyListeners implements Listener {
 			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 300, 4));
 			player.sendMessage("§l[Drugs] §9Giving 15 s of Freeze V and resi V ");
 		}
-        if (name == "§3§lSapho Juice" && Enchantment.getEffects() != null) {
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 2));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600, 5));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 600, 2));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
-        	player.sendMessage("§a[Drugs]§r Time to run ! Gave §b§lSpeed III§r, §7§lWeakness VI§r, and §6§lMining Fatigue III§r for §c§l30 seconds !");
-        	
-        }
-        if (name == "§a§lPlomik Soup" && Enchantment.getEffects() != null) {
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 60, 29));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 400, 2));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 400, 2));
-        	player.sendMessage("§a[Drugs]§r Time to jump ! Gave §a§lJump Boost XXX§r, §2§lNausea III§r, and §8§lBlindness III§r !");
-        }
-        if (name == "§6§lDivine Nectar" && Enchantment.getEffects() != null) {
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 2, 20));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,1200, 0));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 2, 0));
-        	player.sendMessage("§a[Drugs]§r Your Food Bar has been regenerated !");
-        }
-        if (name == "§f§lPale Potion" && Enchantment.getEffects() != null) {
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 36000, 0));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 36000, 1));
-        	player.sendMessage("§a[Drugs]§r You are now completely §7§lInvisible§r !");
-        }
-        if (name == "§6§lSandy's Nap Time" && Enchantment.getEffects() != null) {
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 10, 1));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 10));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 10));
-        	player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 1));
-        	player.sendMessage("§a[Drugs]§r Nap time !");
-        }
-        if (name == "§e§lLucky Drug" && Enchantment.getEffects() != null) {
-        	
-			int randint = (int)(Math.random() * (3 + 1));
-			
+		if (name == "§3§lSapho Juice" && Enchantment.getEffects() != null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 2));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 600, 5));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_DIGGING, 600, 2));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
+			player.sendMessage(
+					"§a[Drugs]§r Time to run ! Gave §b§lSpeed III§r, §7§lWeakness VI§r, and §6§lMining Fatigue III§r for §c§l30 seconds !");
+
+		}
+		if (name == "§a§lPlomik Soup" && Enchantment.getEffects() != null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 60, 29));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 400, 2));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 400, 2));
+			player.sendMessage(
+					"§a[Drugs]§r Time to jump ! Gave §a§lJump Boost XXX§r, §2§lNausea III§r, and §8§lBlindness III§r !");
+		}
+		if (name == "§6§lDivine Nectar" && Enchantment.getEffects() != null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SATURATION, 2, 20));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 1200, 0));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 2, 0));
+			player.sendMessage("§a[Drugs]§r Your Food Bar has been regenerated !");
+		}
+		if (name == "§f§lPale Potion" && Enchantment.getEffects() != null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 36000, 0));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 36000, 1));
+			player.sendMessage("§a[Drugs]§r You are now completely §7§lInvisible§r !");
+		}
+		if (name == "§6§lSandy's Nap Time" && Enchantment.getEffects() != null) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 10, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 10));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 200, 10));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 200, 1));
+			player.sendMessage("§a[Drugs]§r Nap time !");
+		}
+		if (name == "§e§lLucky Drug" && Enchantment.getEffects() != null) {
+
+			int randint = (int) (Math.random() * (3 + 1));
+
 			if (randint == 0) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS ,600 ,6));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 600, 6));
 				player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 2, 1));
 				player.sendMessage("§a[Drugs]§r §4§lBad Luck ...§r Gave you §8§lBlindness VI§r for §b30 seconds§r.");
 			}
-			
+
 			if (randint == 1) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION ,600 ,7));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 600, 7));
 				player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 2, 1));
 				player.sendMessage("§a[Drugs]§r §4§lBad Luck ...§r Gave you §2§lNausea VII§r for §b30 seconds§r.");
 			}
-			
+
 			if (randint == 2) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST,600 ,4));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.HEALTH_BOOST, 600, 4));
 				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 60, 20));
 				player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 2, 1));
 				player.sendMessage("§a[Drugs]§r §a§lLucky !§r Gave you §c§lHealth Boost IV§r for §b30 seconds§r.");
 			}
-			
+
 			if (randint == 3) {
-				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION,600 ,3));
+				player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 3));
 				player.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 2, 1));
 				player.sendMessage("§a[Drugs]§r §a§lLucky !§r Gave you §d§lRegeneration IV§r for §b30 seconds§r.");
 			}
-        }
-    }
-	
+		}
+		if (item.getType() == Material.POTION && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
+				&& item.getItemMeta().getDisplayName().equalsIgnoreCase("§e§lAmbroisie")) {
+			player.addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 2, 10));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 1200, 1));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 600, 0));
+			player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 2, 1));
+			player.sendMessage("§l[Drugs] §aGiving 30 s of Regeneration I, 60 s of Resistance II.");
+		}
+	}
+
 	@EventHandler
 	public void onSplash(PotionSplashEvent event) {
-		
-		if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§6§lBusu§r §aCommon")) {
-			if (event.getAffectedEntities() != null) {
-				List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
-				for (Entity i : list) {
-					
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 40, 10));	
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 60, 3));		
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 80, 3));
-					
-					if (i instanceof Player) {
-						
-						Player player = (Player) i;
-						player.sendMessage("§a[Drugs]§r You've been stunned for §c§l2 seconds§r !");
-						
+		if (event.getEntity() != null) {
+			if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName() != null && event
+					.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§6§lBusu§r §aCommon")) {
+				if (event.getAffectedEntities() != null) {
+					List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
+					for (Entity i : list) {
+
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 40, 10));
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 60, 3));
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 80, 3));
+
+						if (i instanceof Player) {
+
+							Player player = (Player) i;
+							player.sendMessage("§a[Drugs]§r You've been stunned for §c§l2 seconds§r !");
+
+						}
 					}
 				}
 			}
-		}
-		
-		if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§6§lBusu§r §9Rare")) {
-			if (event.getAffectedEntities() != null) {
-				List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
-				for (Entity i : list) {
-					
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 10));	
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 120, 3));		
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 140, 3));
-					
-					if (i instanceof Player) {
-						Player player = (Player) i;
-						player.sendMessage("§a[Drugs]§r You've been stunned for §c§l5 seconds§r !");
-						
+
+			if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName() != null && event
+					.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§6§lBusu§r §9Rare")) {
+				if (event.getAffectedEntities() != null) {
+					List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
+					for (Entity i : list) {
+
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 100, 10));
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, 120, 3));
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 140, 3));
+
+						if (i instanceof Player) {
+							Player player = (Player) i;
+							player.sendMessage("§a[Drugs]§r You've been stunned for §c§l5 seconds§r !");
+
+						}
 					}
 				}
 			}
-		}
-		
-		if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§b§lBleacher")) {
-			
-			if (event.getAffectedEntities()!= null) {
-				List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
-				for (Entity i : list) {
-					for(PotionEffect effect : ((LivingEntity) i).getActivePotionEffects()){
-					    ((LivingEntity) i).removePotionEffect(effect.getType());
+
+			if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName() != null
+					&& event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§b§lBleacher")) {
+
+				if (event.getAffectedEntities() != null) {
+					List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
+					for (Entity i : list) {
+						for (PotionEffect effect : ((LivingEntity) i).getActivePotionEffects()) {
+							((LivingEntity) i).removePotionEffect(effect.getType());
+						}
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100, 1));
+						if (i instanceof Player) {
+							Player player = (Player) i;
+							player.sendMessage("§a[Drugs]§r All your effects have been cleared !");
+							player_list_Bleacher.add(player);
+							@SuppressWarnings("unused")
+							BukkitTask reset_health = new TaskHealthChangeBleacher(this).runTask(Main.getPlugin());
+						}
 					}
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.INVISIBILITY, 100, 1));
-					if (i instanceof Player) {
-						Player player = (Player) i;
-						player.sendMessage("§a[Drugs]§r All your effects have been cleared !");
-						player_list_Bleacher.add(player);
-						@SuppressWarnings("unused")
-						BukkitTask reset_health = new TaskHealthChangeBleacher(this).runTask(Main.getPlugin());
+				}
+
+			}
+
+			if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName() != null
+					&& event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§2§lRotten Heal")) {
+				if (event.getAffectedEntities() != null) {
+					List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
+					for (Entity i : list) {
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 300, 1));
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.POISON, 300, 2));
+						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
+						if (i instanceof Player) {
+							Player player = (Player) i;
+							player.sendMessage("§a[Drugs]§r You've been debuffed !");
+							player_list_RottenHeal.add(player);
+							player.setMaxHealth(16.0);
+							player.setHealth(16.0);
+							@SuppressWarnings("unused")
+							BukkitTask reset_health = new TaskHealthChangeRottenHeal(this)
+									.runTaskLater(Main.getPlugin(), 300L);
+						}
 					}
 				}
 			}
-			
-		}
-		
-		if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§2§lRotten Heal")) {
-			if (event.getAffectedEntities() != null) {
-				List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
-				for (Entity i : list) {
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 300, 1));
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.POISON, 300, 2));	
-					((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.CONFUSION, 120, 5));
-					if (i instanceof Player) {
-						Player player = (Player) i;
-						player.sendMessage("§a[Drugs]§r You've been debuffed !");
-						player_list_RottenHeal.add(player);
-						player.setMaxHealth(16.0);
-						player.setHealth(16.0);
-						@SuppressWarnings("unused")
-						BukkitTask reset_health = new TaskHealthChangeRottenHeal(this).runTaskLater(Main.getPlugin(), 300L);
-					}
-				}
-			}
-		}
-		
-		if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§d§lDouble Heal")) {
-			if (event.getAffectedEntities() != null) {
-				List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
-				for (Entity i : list) {
-					if (i instanceof Player) {
-						Player player = (Player) i;
-						player.sendMessage("§a[Drugs]§r You've been double-healed !");
-						((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 2));
+
+			if (event.getPotion() != null && event.getPotion().getItem().getItemMeta().getDisplayName() != null
+					&& event.getPotion().getItem().getItemMeta().getDisplayName().equalsIgnoreCase("§d§lDouble Heal")) {
+				if (event.getAffectedEntities() != null) {
+					List<Entity> list = new ArrayList<Entity>(event.getAffectedEntities());
+					for (Entity i : list) {
+						if (i instanceof Player) {
+							Player player = (Player) i;
+							player.sendMessage("§a[Drugs]§r You've been double-healed !");
+							((LivingEntity) i).addPotionEffect(new PotionEffect(PotionEffectType.HEAL, 1, 2));
+						}
 					}
 				}
 			}
